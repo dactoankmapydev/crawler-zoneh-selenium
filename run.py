67a9ab254e8bacfb41b15e7bef06713ed1d940c8
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-20 18:38:50 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import sys
 from time import sleep
 
 from crawler.zone_h import connect_to_url, get_driver, parse_html
@@ -7,22 +8,29 @@ def run_process(page_number, browser):
     if connect_to_url(browser, page_number):
         sleep(2)
         html = browser.page_source
-        output = parse_html(html)
+        parse_html(html)
     else:
         print("Error connecting to zone-h")
 
 
 if __name__ == '__main__':
+    # headless mode
+    headless = False
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "headless":
+            print("Running in headless mode")
+            headless = True
+
     current_page = 1
 
     # init browser
-    browser = get_driver()
+    browser = get_driver(headless=headless)
 
     # crawler
-    while current_page <= 30:
+    while current_page <= 50:
         print(f"Scraping page #{current_page}...")
         run_process(current_page, browser)
-        current_page = current_page + 1
+        current_page += 1
 
     # exit
     browser.quit()
